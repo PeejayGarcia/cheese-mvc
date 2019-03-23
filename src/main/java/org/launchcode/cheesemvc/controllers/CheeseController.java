9c1eafa5efa4f466e76fe77c36619ad34044926b
Peejay Garcia <peejgarcia@gmail.com>
2019-03-23 12:50:30 -0500

    Restarted building the app using correct Spring version.

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,16 @@
+package org.launchcode.cheesemvc.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index() {
+        return "cheese/index";
+    }
+}
